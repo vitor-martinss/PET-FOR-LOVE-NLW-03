1b5b1b413af311a10ea95ff802d7f3734dc1c63c
Vitor Martins <vitor.martins@fever.pt>
2020-10-15 20:14:17 +0100

    UPD front end web

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -44,7 +44,7 @@ function OrphanagesMap() {
 					<span>Portugal</span>
 				</footer>
 			</aside>
-			<Map center={[38.7241156, -9.1345474]} zoom={15} style={{ width: '100%', height: '100%' }}>
+			<Map center={[38.7241156, -9.1345474]} zoom={13} style={{ width: '100%', height: '100%' }}>
 				{/* <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png' /> */}
 				<TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
 				{guilds.map(guild => {
