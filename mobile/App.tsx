f1ad137be454b69f3678c8043e32a1638d5c35f3
Vitor Martins <vitor.martins@fever.pt>
2020-10-16 18:21:30 +0100

    ADD mobile screens front-end

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,42 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 
-import mapMarker from './src/images/map-marker.png'
+import { useFonts } from 'expo-font'
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
+
+import Routes from './src/routes'
 
 export default function App() {
+
+	const [fontsLoaded] = useFonts({
+		Nunito_600SemiBold, 
+		Nunito_700Bold, 
+		Nunito_800ExtraBold
+	})
+
+	if(!fontsLoaded) {
+		return null
+	}
+
 	return (
-		<View style={styles.container}>
-			
-			<MapView 
-				provider={PROVIDER_GOOGLE}
-				style={styles.map} 
-				initialRegion={{
-					latitude: 38.7241156, 
-					longitude: -9.1345474,
-					latitudeDelta: 0.008,
-					longitudeDelta: 0.008,
-				}} 
-			>
-				<Marker
-					icon={mapMarker}
-					coordinate={{
-						latitude: 38.7241156, 
-						longitude: -9.1345474,
-					}}
-				/>
-			</MapView>
-		</View>
-	);
+		<Routes />
+	)
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-
-	map: {
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height
-	}
-});
